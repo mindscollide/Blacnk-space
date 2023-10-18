f8402b503727610a2f3c4dd377488deb78a02813
Huzeifa Jahangir <84620132+huzeifajaha@users.noreply.github.com>
2023-10-18 10:38:34 +0500

    chnages

--- src/container/Categories/CategoryDetails.js
+++ src/container/Categories/CategoryDetails.js
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Button, Loader, StarRating } from "./../../components/Elements";
 import { useNavigate } from "react-router-dom";
@@ -15,52 +15,45 @@ import { getRndomeNumber } from "../../common/Function/utils";
 
 const CategoryDetails = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const businessListing = useSelector(
     (state) => state.actionReducer.businessListing
   );
 
-  const dispatch = useDispatch();
-  //state for businessDetails
-  const [businessDetails, setBusinessDetails] = useState([]);
-
-  // STATE FOR LOADER
-  const [showLoader, setShowLoader] = useState(false);
-
-  // state for businessDetail page
-  const [stateBusinessDetail, setStateBusinessDetails] = useState({
-    BusinessListingID: {
-      value: "",
-      errorMessage: "",
-      errorStatus: false,
-    },
-  });
+  const [businessDetails, setBusinessDetails] = useState({});
+  const [showLoader, setShowLoader] = useState(true);
 
   useEffect(() => {
-    if (businessListing !== null && businessListing !== undefined) {
+    if (businessListing) {
       setBusinessDetails(businessListing);
+      setShowLoader(false);
+      console.log("helloooooo 1");
     }
   }, [businessListing]);
 
   const clickHomeHandler = () => {
     navigate("/BlankSpace/");
   };
-  useEffect(() => {
-    if (performance.navigation.type === performance.navigation.TYPE_RELOAD) {
-      const storedData = localStorage.getItem("newBusinessIdData");
-      console.log("helloooooo");
-      // Parse the JSON string into an object
-      const newBusinessIdData = JSON.parse(storedData);
 
-      dispatch(businessDetailsMainApi(navigate, newBusinessIdData));
-    } else {
-    }
+  useEffect(() => {
+    // if (performance.navigation.type === performance.navigation.TYPE_RELOAD) {
+    const storedData = localStorage.getItem("newBusinessIdData");
+    const newBusinessIdData = JSON.parse(storedData);
+    console.log("helloooooo");
+    dispatch(businessDetailsMainApi(navigate, newBusinessIdData));
+    // }
   }, []);
 
   return (
     <Fragment>
-      {Object.keys(businessDetails).length > 0 ? (
-        <>
-          {Object.keys(businessDetails.listOfBase64Images).length > 0 ? (
+      {showLoader && (
+        <div className="loader-overlay">
+          <Loader />
+        </div>
+      )}
+      {Object.keys(businessDetails).length > 0 && (
+        <Container>
+          {Object.keys(businessDetails.listOfBase64Images).length > 0 && (
             <div className="swiper-category-slider">
               <Row>
                 <Col lg={12} md={12} sm={12}>
@@ -92,52 +85,49 @@ const CategoryDetails = () => {
                 </Col>
               </Row>
             </div>
-          ) : (
-            <></>
           )}
-
-          <Container>
-            <Row
-              className={`${
-                Object.keys(businessDetails.listOfBase64Images).length > 0
-                  ? "mb-4 margin-top-403"
-                  : "mb-4"
-              }`}
-            >
-              <Col lg={6} md={6} sm={6} xs={6} className="mt-2">
-                <span className="title-emirates-heading">
-                  {businessDetails.name}
-                </span>
-                <Row className="mb-3">
-                  <Col lg={6} md={6} sm={12} xs={12}>
-                    <span className="show-inline-display">
-                      <StarRating
-                        className="star-font-size"
-                        rating={businessDetails.rating}
-                      />
-                      <span className="Reviews-after-star">
-                        ({businessDetails.reviewsCount} Reviews)
-                      </span>
-                    </span>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col lg={6} md={6} sm={6} xs={6}>
-                    <Button
-                      text={businessDetails.categoryName}
-                      className="Tour-button"
+          <Row
+            className={`${
+              Object.keys(businessDetails.listOfBase64Images).length > 0
+                ? "mb-4 margin-top-403"
+                : "mb-4"
+            }`}
+          >
+            <Col lg={6} md={6} sm={6} xs={6} className="mt-2">
+              <span className="title-emirates-heading">
+                {businessDetails.name}
+              </span>
+              <Row className="mb-3">
+                <Col lg={6} md={6} sm={12} xs={12}>
+                  <span className="show-inline-display">
+                    <StarRating
+                      className="star-font-size"
+                      rating={businessDetails.rating}
                     />
-                  </Col>
-                </Row>
-              </Col>
-              <Col
-                lg={6}
-                md={6}
-                sm={6}
-                xs={6}
-                className="package-row-class flex-row"
-              >
-                {businessDetails.packageStarts !== "" ? (
+                    <span className="Reviews-after-star">
+                      ({businessDetails.reviewsCount} Reviews)
+                    </span>
+                  </span>
+                </Col>
+              </Row>
+              <Row>
+                <Col lg={6} md={6} sm={6} xs={6}>
+                  <Button
+                    text={businessDetails.categoryName}
+                    className="Tour-button"
+                  />
+                </Col>
+              </Row>
+            </Col>
+            <Col
+              lg={6}
+              md={6}
+              sm={6}
+              xs={6}
+              className="package-row-class flex-row"
+            >
+              {businessDetails.packageStarts &&
+                businessDetails.packageStarts !== "" && (
                   <>
                     <span className="verticalline"></span>
                     <div className="line-height">
@@ -152,171 +142,64 @@ const CategoryDetails = () => {
                       </div>
                     </div>
                   </>
-                ) : (
-                  <>
-                    <div style={{ display: "none" }}></div>
-                  </>
                 )}
-              </Col>
-            </Row>
+            </Col>
+          </Row>
 
-            {/* section for detail contact and location */}
-
-            <Row>
-              <Col lg={12} md={12} sm={12}>
-                <p className="para-text">{businessDetails.aboutUs}</p>
-              </Col>
-            </Row>
-            <Row>
-              <Col lg={12} md={12} sm={12} className="Grourp-btn-col">
+          <Row>
+            <Col lg={12} md={12} sm={12}>
+              <p className="para-text">{businessDetails.aboutUs}</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={12} md={12} sm={12} className="Grourp-btn-col">
+              <Button
+                icon={<i className="icon-home call-etc-icons-sizes"></i>}
+                onClick={clickHomeHandler}
+                className="buttons-call-etc"
+              />
+              {businessDetails.contactNumber ? (
                 <Button
-                  icon={<i className="icon-home call-etc-icons-sizes"></i>}
-                  onClick={clickHomeHandler}
+                  icon={<i className="icon-call call-etc-icons-sizes"></i>}
                   className="buttons-call-etc"
+                  onClick={() =>
+                    window.open(businessDetails.contactNumber, "_blank")
+                  }
                 />
-                {businessDetails.contactNumber !== "" ? (
-                  <>
-                    <Button
-                      icon={<i className="icon-call call-etc-icons-sizes"></i>}
-                      className="buttons-call-etc"
-                      // onClick={() =>
-                      //   window.open(businessDetails.contactNumber, "_blank")
-                      // }
-                    ></Button>
-                  </>
-                ) : (
-                  <>
-                    <Button
-                      icon={<i className="icon-call call-etc-icons-sizes"></i>}
-                      className="buttons-disable-etc"
-                    ></Button>
-                  </>
-                )}
-                {businessDetails.location !== "" ? (
-                  <>
-                    <Button
-                      icon={
-                        <i className="icon-location  call-etc-icons-sizes"></i>
-                      }
-                      className="buttons-call-etc"
-                      // onClick={() =>
-                      //   window.open(businessDetails.location, "_blank")
-                      // }
-                    >
-                      {/* {businessDetails.location} */}
-                    </Button>
-                  </>
-                ) : (
-                  <>
-                    <Button
-                      icon={
-                        <i className="icon-location  call-etc-icons-sizes"></i>
-                      }
-                      className="buttons-disable-etc"
-                    ></Button>
-                  </>
-                )}
+              ) : (
                 <Button
-                  icon={<i className="icon-web call-etc-icons-sizes"></i>}
-                  onClick={() => window.open(businessDetails.website, "_blank")}
+                  icon={<i className="icon-call call-etc-icons-sizes"></i>}
+                  className="buttons-disable-etc"
+                />
+              )}
+              {businessDetails.location ? (
+                <Button
+                  icon={<i className="icon-location call-etc-icons-sizes"></i>}
                   className="buttons-call-etc"
-                />{" "}
-              </Col>
-            </Row>
-
-            {/* section for what we offer */}
-
-            <Row className="mt-5">
-              <Col
-                lg={12}
-                md={12}
-                sm={12}
-                className="d-flex justify-content-start"
-              >
-                {businessDetails.offering !== null ? (
-                  <>
-                    <span className="what-we-offer-heading">WHAT WE OFFER</span>
-                  </>
-                ) : (
-                  <>
-                    <Row>
-                      <Col>
-                        <div style={{ diplay: "none" }}></div>
-                      </Col>
-                    </Row>
-                  </>
-                )}
-              </Col>
-            </Row>
-
-            <Row>
-              <Col
-                lg={12}
-                md={12}
-                sm={12}
-                className="d-flex justify-content-start"
-              >
-                <div className="what-we-offer-bullets">
-                  <span className="what-we-offer-subtitles">
-                    {businessDetails.offering}
-                  </span>
-                </div>
-              </Col>
-            </Row>
-
-            {/* section for Here's what are customers have to say */}
-
-            <Row className="mt-5">
-              <Col
-                lg={12}
-                md={12}
-                sm={12}
-                className="d-flex justify-content-start"
-              >
-                {businessDetails.reviews !== null ? (
-                  <>
-                    <span className="what-we-offer-heading">
-                      Here's what are customers have to say
-                    </span>
-                  </>
-                ) : (
-                  <>
-                    <div style={{ display: "none" }}></div>
-                  </>
-                )}
-              </Col>
-            </Row>
-
-            <Row>
-              <Col
-                lg={12}
-                md={12}
-                sm={12}
-                className="d-flex justify-content-start"
-              >
-                <div className="what-we-offer-bullets ">
-                  <span className="what-we-offer-subtitles">
-                    {businessDetails.reviews}
-                  </span>
-                </div>
-              </Col>
-            </Row>
-          </Container>
+                  onClick={() =>
+                    window.open(businessDetails.location, "_blank")
+                  }
+                />
+              ) : (
+                <Button
+                  icon={<i className="icon-location call-etc-icons-sizes"></i>}
+                  className="buttons-disable-etc"
+                />
+              )}
+              <Button
+                icon={<i className="icon-web call-etc-icons-sizes"></i>}
+                onClick={() => window.open(businessDetails.website, "_blank")}
+                className="buttons-call-etc"
+              />
+            </Col>
+          </Row>
 
           <Row className="mt-5">
             <Col lg={12} md={12} sm={12}>
               <Footer operationHours={businessDetails.operationHours} />
             </Col>
           </Row>
-        </>
-      ) : (
-        <></>
-      )}
-
-      {showLoader && (
-        <div className="loader-overlay">
-          <Loader />
-        </div>
+        </Container>
       )}
     </Fragment>
   );
