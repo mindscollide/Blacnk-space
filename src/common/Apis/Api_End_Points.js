ede866566cecbbc3d11645a19d56f29c90595628
aunnaqvi <aunnaqvi747@gmail.com>
2023-10-19 10:17:29 +0500

    push
    push 11

--- src/common/Apis/Api_End_Points.js
+++ src/common/Apis/Api_End_Points.js
@@ -1,8 +1,8 @@
 // our base url or machine api For Blancspace
-const baseURL = "https://app.blancspace.net";
+const baseURL = "http://192.168.18.241";
 
 // our service URLs
-const authenticationPort = "/DAuth/";
+const authenticationPort = ":12000/AUTH";
 //our Final Api
 const authenticationAPI = baseURL + authenticationPort;
 
